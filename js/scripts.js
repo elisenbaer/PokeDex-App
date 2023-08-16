6b897bc16ab287bb6ac9e1f5c55610da57196517
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-08-16 15:16:07 -0700

    add spaces for readability

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ let pokemonList = [
     { name: "Pikachu", height: 4, types: ["static", "lightningrod"] },
     { name: "Dewgong", height: 17, types: ["thick-fat", "hydration", "ice-body"] },
 ];
+
+
  for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name);
     document.write(pokemonList[i].height);
+
+
