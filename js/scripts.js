bb4ad47413c0cbcb5d23954f484964a6283b7155
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-08-23 15:06:05 -0700

    replace <br> with "<p>" + "</p>"

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,11 @@ let pokemonList = [
  for (let i = 0; i < pokemonList.length; i++){
     document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].types}) <br><br>`);
     if (pokemonList[i].height > 10){
-        document.write("This Pokemon is so big!<br><br>");
+        document.write("<p>" + "This Pokemon is so big!" + "</p>");
      }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
-        document.write("This Pokemon is average.<br><br>");
+        document.write("<p>" + "This Pokemon is average." + "</p>");
      }else {
-        document.write("This Pokemon is small.<br><br>");
+        document.write("<p>" + "This Pokemon is small.<br><br><br>" + "</p>");
      }   
 }
 
