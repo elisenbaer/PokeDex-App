a6030d7b6ab1ac5d14e22369573941b98ca49d05
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-08-22 17:16:49 -0700

    add breaks after conditionals so doc flows better

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,11 @@ let pokemonList = [
  for (let i = 0; i < pokemonList.length; i++){
     document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].types}) <br><br>`);
     if (pokemonList[i].height > 10){
-        document.write("This Pokemon is so big!");
+        document.write("This Pokemon is so big!<br><br>");
      }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
-        document.write("This Pokemon is average.");
+        document.write("This Pokemon is average.<br><br>");
      }else {
-        document.write("This Pokemon is small.");
+        document.write("This Pokemon is small.<br><br>");
      }   
 }
 
