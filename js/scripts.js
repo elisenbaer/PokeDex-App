288f1551e73f14567af3083d9abfb004ed326348
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-08-31 18:12:32 -0700

    create IIFE or pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
-let pokemonList = [
+let pokemonRepository = (function() {
+   let pokemonList = [
     { 
       name: "Bulbasaur", 
       height: 7, 
@@ -8,13 +9,14 @@ let pokemonList = [
     { 
       name: "Pikachu",
       height: 4, 
+      types: ["electric"] 
     },
     { 
       name: "Dewgong", 
       height: 17, 
       types: ["water", "ice"] 
     },
-];
+   ];
 
 
 // for (let i = 0; i < pokemonList.length; i++){
