6eff410aaa79c7ff52db9b0f52d953120c8bc36e
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-08-31 18:11:50 -0700

    fix indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 
 let pokemonList = [
     { 
-    name: "Bulbasaur", 
-    height: 7, 
-    types: ["grass", "poison"] 
+      name: "Bulbasaur", 
+      height: 7, 
+      types: ["grass", "poison"] 
     },
     { 
-    name: "Pikachu",
-    height: 4, 
-    types: ["electric"] 
+      name: "Pikachu",
+      height: 4, 
     },
     { 
-    name: "Dewgong", 
-    height: 17, 
-    types: ["water", "ice"] 
+      name: "Dewgong", 
+      height: 17, 
+      types: ["water", "ice"] 
     },
 ];
 
@@ -21,15 +20,15 @@ let pokemonList = [
 // for (let i = 0; i < pokemonList.length; i++){
 //    document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].types}) <br><br>`);
 //   if (pokemonList[i].height > 10){
-//      document.write("<p>" + "This Pokemon is so big!" + "</p>");
-//    }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
-//      document.write("<p>" + "This Pokemon is average." + "</p>");
-//    }else {
-//      document.write("<p>" + "This Pokemon is small." + "</p>");
-//    }   
+//          document.write("<p>" + "This Pokemon is so big!" + "</p>");
+//       }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
+//          document.write("<p>" + "This Pokemon is average." + "</p>");
+//       }else {
+//          document.write("<p>" + "This Pokemon is small." + "</p>");
+//       }   
 // }
 
-pokemonList.forEach(function(value) {
+pokemonRepository.getAll().forEach(function(value) {
       document.write(`${value.name} <br> (height: ${value.height}) <br> (type: ${value.types}) <br><br>`);
       if (value.height > 10){
             document.write("<p>" + "This Pokemon is so big!" + "</p>");
