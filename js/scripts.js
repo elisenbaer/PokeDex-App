029cbfcfff5711424e9321ead830a6361f881c7d
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-09-12 12:10:25 -0700

    delete the forEach loop from js

--- js/scripts.js
+++ js/scripts.js
@@ -39,24 +39,4 @@ pokemonRepository.getAll();
 pokemonRepository.add(pokemon);
 
 
-// for (let i = 0; i < pokemonList.length; i++){
-//    document.write(`${pokemonList[i].name} <br> (height: ${pokemonList[i].height}) <br> (type: ${pokemonList[i].types}) <br><br>`);
-//   if (pokemonList[i].height > 10){
-//          document.write("<p>" + "This Pokemon is so big!" + "</p>");
-//       }else if (pokemonList[i].height < 10 && pokemonList[i].height > 5){
-//          document.write("<p>" + "This Pokemon is average." + "</p>");
-//       }else {
-//          document.write("<p>" + "This Pokemon is small." + "</p>");
-//       }   
-// }
-
-pokemonRepository.getAll().forEach(function(value) {
-      document.write(`${value.name} <br> (height: ${value.height}) <br> (type: ${value.types}) <br><br>`);
-      if (value.height > 10){
-            document.write("<p>" + "This Pokemon is so big!" + "</p>");
-         }else if (value.height < 10 && value.height > 5){
-            document.write("<p>" + "This Pokemon is average." + "</p>");
-         }else {
-            document.write("<p>" + "This Pokemon is small." + "</p>");
-         }   
 })
