a3721ee9a58e234f421a2039c25934df3006637b
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-09-13 14:46:08 -0700

    reformat to correct indentations and move to be synchronous

--- js/scripts.js
+++ js/scripts.js
@@ -3,58 +3,60 @@ let pokemonRepository = (function() {
    let pokemonList = [];
    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
    
-         function getAll() {
+   function add(pokemon) {
+      if (
+         typeof pokemon === "object" && "name" in pokemon
+      ) {
+         pokemonList.push(pokemon);
+      }  else {
+         console.log("pokemon is not correct");
+      }
+   }      
+   
+   function getAll() {
             return pokemonList;
          }
 
-         function addListItem(pokemon){
-            let pokemonList = document.querySelector('.pokemon-list');
-            let listPokemon = document.createElement('li');
-            let button = document.createElement('button');
-            button.innerText = pokemon.name;
-            button.classList.add('button-class');
-            listPokemon.appendChild(button);
-            pokemonList.appendChild(listPokemon);
-            button.addEventListener('click', function(showDetails) {
-               console.log(pokemon.name)
-            });
-         }
+   function addListItem(pokemon){
+      let pokemonList = document.querySelector('.pokemon-list');
+      let listPokemon = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listPokemon.appendChild(button);
+      pokemonList.appendChild(listPokemon);
+      button.addEventListener('click', function(showDetails) {
+         console.log(pokemon.name)
+      });
+   }
 
-         function showDetails(pokemon){
-            console.log(pokemon.name)
-         }
+   function showDetails(pokemon){
+      console.log(pokemon.name)
+   }
 
-         function add(pokemon) {
-            if (
-               typeof pokemon === "object" && "name" in pokemon
-            ) {
-               repository.push(pokemon);
-            }  else {
-               console.log("pokemon is not correct");
-            }
-         }
-         
-         function loadList() {
-            return fetch(apiUrl).then(function (response) {
-               return response.json();
-            }).then(function (json) {
-               json.results.forEach(function (item) {
-                  let pokemon = {
-                     name: item.name,
-                     detailsUrl: item.url
-                  };
-                  add(pokemon);
-               });
-            }).catch(function (e) {
-               console.error(e);
-            })
-         }
 
-         return {
-            getAll: getAll,
-            add: add,
-            addListItem: addListItem
-         };
+   
+   function loadList() {
+      return fetch(apiUrl).then(function (response) {
+         return response.json();
+      }).then(function (json) {
+         json.results.forEach(function (item) {
+            let pokemon = {
+               name: item.name,
+               detailsUrl: item.url
+            };
+            add(pokemon);
+         });
+      }).catch(function (e) {
+         console.error(e);
+      })
+   }
+
+   return {
+      getAll: getAll,
+      add: add,
+      addListItem: addListItem
+   };
 
 })();
 
