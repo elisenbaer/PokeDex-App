c307c0815af6ab2465327daf4289ac26d2b080b5
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-09-13 15:23:52 -0700

    call new showDetails function with parameter 'pokemon' to include loadDetails promise and log it all to console

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,12 @@ let pokemonRepository = (function() {
       });
    }
 
+   function showDetails(pokemon) {
+      loadDetails(pokemon).then(function () {
+         console.log(pokemon);
+      });
+   }
+
    return {
       getAll: getAll,
       add: add,
