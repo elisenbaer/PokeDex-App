54f988037768071b27f7651e842ffce978b10c1b
elisenbaer <140930219+elisenbaer@users.noreply.github.com>
2023-09-13 14:35:55 -0700

    remove individual pokemon from repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,5 @@
 
 let pokemonRepository = (function() {
-   let pokemonList = [
-    { 
-      name: "Bulbasaur", 
-      height: 7, 
-      types: ["grass", "poison"] 
-    },
-    { 
-      name: "Pikachu",
-      height: 4, 
-      types: ["electric"] 
-    },
-    { 
-      name: "Dewgong", 
-      height: 17, 
-      types: ["water", "ice"] 
-    },
-   ];
    
          function getAll() {
             return pokemonList;
